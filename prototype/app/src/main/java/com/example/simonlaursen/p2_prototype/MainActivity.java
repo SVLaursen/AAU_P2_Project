cf31ab598f1daf865dfd498d0965d3c11828b05b
Simon Laursen <>
2018-04-11 13:55:34 +0200

    Wireframe setup

--- prototype/app/src/main/java/com/example/simonlaursen/p2_prototype/MainActivity.java
+++ prototype/app/src/main/java/com/example/simonlaursen/p2_prototype/MainActivity.java
@@ -2,14 +2,16 @@ package com.example.simonlaursen.p2_prototype;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
+    private boolean isActive; //Used to change between workout activated and deactivated
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,16 +19,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //INCOMING BUTTON SETUP
-        Button home = findViewById(R.id.HomeButton);
-        Button calendar = findViewById(R.id.CalendarButton);
-        Button profile = findViewById(R.id.ProfileButton);
-        Button inputExercise = findViewById(R.id.ExerciseInput);
-        Button inputInsulin = findViewById(R.id.InsulinInput);
+        ImageButton home = findViewById(R.id.HomeButton);
+        ImageButton calendar = findViewById(R.id.CalendarButton);
+        ImageButton profile = findViewById(R.id.ProfileButton);
+        ImageButton inputExercise = findViewById(R.id.ExerciseInput);
+        ImageButton inputInsulin = findViewById(R.id.InsulinInput);
+        final ImageButton startWorkout = findViewById(R.id.centerButton); //Test button
         //END BUTTON SETUP
 
-        //TODO: Add in a check if the current activity is running and if it is then don't go to it
-        //TODO: Kill off other activities when they are left
+
         //TODO: OOP the button process.
+        //TODO: Make a functioning calendar
+        //TODO: Implement the final graphical elements
+        //TODO: Finish up profile activity and design for drop down menus
 
 
         //INCOMING BUTTON ACTIONS
@@ -40,8 +45,6 @@ public class MainActivity extends AppCompatActivity {
         calendar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //Toast.makeText(getApplicationContext(), "Calendar button pressed", Toast.LENGTH_SHORT).show();
-
                 Intent intent = new Intent(v.getContext(), CalendarActivity.class);
                 intent.getStringExtra("key");
                 startActivity(intent);
@@ -51,23 +54,31 @@ public class MainActivity extends AppCompatActivity {
         profile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //Toast.makeText(getApplicationContext(),"Profile button pressed",Toast.LENGTH_SHORT).show();
-
                 Intent intent = new Intent(v.getContext(),ProfileActivity.class); //Go to profile activity
                 intent.getStringExtra("key");
                 startActivity(intent);
-                //finish();
             }
         });
 
         inputExercise.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //Toast.makeText(getApplicationContext(),"Input Exercise pressed",Toast.LENGTH_SHORT).show();
+                AlertDialog.Builder mBuilder = new AlertDialog.Builder(MainActivity.this, R.style.AppTheme_DialogTheme);
+                View mView = getLayoutInflater().inflate(R.layout.dialogue_input_exercise,null);
+                //final EditText mTime = mView.findViewById(R.id.yourTime);
 
-                Intent intent = new Intent(v.getContext(), InputExerciseActivity.class);
-                intent.getStringExtra("key");
-                startActivityForResult(intent,0);
+                mBuilder.setView(mView);
+                final AlertDialog dialog = mBuilder.create();
+                dialog.show();
+                dialog.setCanceledOnTouchOutside(true);
+
+                ImageButton backButton = mView.findViewById(R.id.backButton);
+
+                backButton.setOnClickListener(new View.OnClickListener(){
+                    public void onClick(View v){
+                        dialog.cancel();
+                    }
+                });
             }
         });
 
@@ -75,6 +86,39 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Toast.makeText(getApplicationContext(),"Input Insulin pressed",Toast.LENGTH_SHORT).show();
+
+                AlertDialog.Builder mBuilder = new AlertDialog.Builder(MainActivity.this, R.style.AppTheme_DialogTheme);
+                View mView = getLayoutInflater().inflate(R.layout.dialogue_input_insulin,null);
+                //final EditText mInsulin = mView.findViewById(R.id.inputInsulin);
+
+                mBuilder.setView(mView);
+                final AlertDialog dialog = mBuilder.create();
+                dialog.show();
+                dialog.setCanceledOnTouchOutside(true);
+
+                ImageButton backButton = mView.findViewById(R.id.backButton);
+
+                backButton.setOnClickListener(new View.OnClickListener(){
+                    public void onClick(View v){
+                        dialog.cancel();
+                    }
+                });
+            }
+        });
+
+        startWorkout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!isActive){
+                    startWorkout.setImageResource(R.drawable.main_center_clicked);
+                    Toast.makeText(getApplicationContext(),"You've started workout",Toast.LENGTH_SHORT).show();
+                    isActive = true;
+                }
+                else{
+                    startWorkout.setImageResource(R.drawable.main_center_unclicked);
+                    Toast.makeText(getApplicationContext(),"Workout stopped",Toast.LENGTH_SHORT).show();
+                    isActive = false;
+                }
             }
         });
 
