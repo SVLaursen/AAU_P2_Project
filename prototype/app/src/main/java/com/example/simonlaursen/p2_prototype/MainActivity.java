239df6f86816311db96f9b1cdd672b77d3e1708e
Simon Laursen <>
2018-04-23 19:10:35 +0200

    Reworked to now use fragments instead of activities for screens

--- prototype/app/src/main/java/com/example/simonlaursen/p2_prototype/MainActivity.java
+++ prototype/app/src/main/java/com/example/simonlaursen/p2_prototype/MainActivity.java
@@ -5,12 +5,19 @@ import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    /*
+    Okay, so this is the main activity, which will run the whole thing!
+    The program itself, such as the home screen, calendar screen, etc. will all be fragments loaded up inside this.
+    The reason why we do this is because it'll be much easier to do hotfixes along the way, as well as accessing the data we need.
+     */
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //TODO: Button layout
 
     }
+
 }
