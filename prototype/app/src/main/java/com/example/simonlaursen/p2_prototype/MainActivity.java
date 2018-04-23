7ec6d132aa46d9778e023fa152b68067feba1f32
Simon Laursen <>
2018-04-23 20:55:38 +0200

    Implemented Nav bar buttons, including a slight tactile feedback for the user through vibration.

--- prototype/app/src/main/java/com/example/simonlaursen/p2_prototype/MainActivity.java
+++ prototype/app/src/main/java/com/example/simonlaursen/p2_prototype/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.simonlaursen.p2_prototype;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,12 +17,59 @@ public class MainActivity extends AppCompatActivity {
      */
 
 
+    //GLOBAL VARIABLES HERE!
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        NavBarSetup(); //Used to setup the size of the navigation bar
+    }
 
+    //NAV BAR
+    private void NavBarSetup(){
+
+        //TODO: Load up the different fragments!
+
+        ImageButton homeButton = findViewById(R.id.HomeButton);
+        ImageButton profileButton = findViewById(R.id.ProfileButton);
+        ImageButton calendarButton = findViewById(R.id.CalendarButton);
+
+        final Vibrator vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+
+        homeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"You pressed home", Toast.LENGTH_SHORT).show();
+
+                if(vibrator.hasVibrator()){
+                    vibrator.vibrate(10);
+                }
+            }
+        });
+
+        profileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"You pressed profile", Toast.LENGTH_SHORT).show();
+
+                if(vibrator.hasVibrator()){
+                    vibrator.vibrate(10);
+                }
+            }
+        });
+
+        calendarButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"You pressed calendar",Toast.LENGTH_SHORT).show();
+
+                if(vibrator.hasVibrator()){
+                    vibrator.vibrate(10);
+                }
+            }
+        });
     }
 
 }
