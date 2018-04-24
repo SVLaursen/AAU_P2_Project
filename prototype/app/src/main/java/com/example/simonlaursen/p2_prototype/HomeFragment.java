05d822c38816df3711bffc20fbe8218d8d2f40f9
Simon Laursen <>
2018-04-24 12:03:27 +0200

    Buttons has been setup!

--- prototype/app/src/main/java/com/example/simonlaursen/p2_prototype/HomeFragment.java
+++ prototype/app/src/main/java/com/example/simonlaursen/p2_prototype/HomeFragment.java
@@ -1,22 +1,58 @@
 package com.example.simonlaursen.p2_prototype;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.os.Vibrator;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 public class HomeFragment extends Fragment {
 
 
     public HomeFragment() {
+
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View v = inflater.inflate(R.layout.fragment_home,null);
+        InputButtons(v);
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, null);
+        return v;
+    }
+
+    private void InputButtons(View v){
+        Fragment fragment = this;
+
+        ImageButton inputExercise = v.findViewById(R.id.InputExercise);
+        ImageButton inputInsulin = v.findViewById(R.id.InputInsulin);
+
+        final Vibrator vibrator = (Vibrator) fragment.getActivity().getSystemService(Context.VIBRATOR_SERVICE);
+
+        inputExercise.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(vibrator.hasVibrator()){
+                    vibrator.vibrate(10);
+                }
+            }
+        });
+
+        inputInsulin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(vibrator.hasVibrator()){
+                    vibrator.vibrate(10);
+                }
+            }
+        });
     }
 
 }
