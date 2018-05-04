152c0bf5b119e723a1890c1baeff41ecc168ec2d
EmilValberg <36535221+EmilValberg@users.noreply.github.com>
2018-05-04 10:37:31 +0200

    Calendar

--- prototype/app/src/main/java/com/example/simonlaursen/p2_prototype/CalendarFragment.java
+++ prototype/app/src/main/java/com/example/simonlaursen/p2_prototype/CalendarFragment.java
@@ -1,10 +1,16 @@
 package com.example.simonlaursen.p2_prototype;
 
 
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
+import android.text.style.ForegroundColorSpan;
+import android.text.style.RelativeSizeSpan;
+import android.text.style.StyleSpan;
+import android.text.style.TextAppearanceSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -39,7 +45,7 @@ public class CalendarFragment extends Fragment {
 
     public void calendarSetup(View v){
         MaterialCalendarView materialCalendarView = (MaterialCalendarView) v.findViewById(R.id.calendarView);
-        //WeekDayFormatter weekDayFormatter = (WeekDayFormatter) v.findViewById(R.id.calendarView);
+        final View view = v;
 
         materialCalendarView.state().edit()
                 .setFirstDayOfWeek(Calendar.MONDAY)
@@ -50,10 +56,32 @@ public class CalendarFragment extends Fragment {
 
         materialCalendarView.setTopbarVisible(true);
         materialCalendarView.getSelectedDate();
+        materialCalendarView.setWeekDayLabels(new String[] {"SØN", "MAN", "TIR","ONS","TOR","FRE","LØR"}); //Sets the labels for the weekdays.
 
+        //sets the selected day to the current day, when entering the calendar
         Calendar calendar = Calendar.getInstance();
         materialCalendarView.setDateSelected(calendar.getTime(), true);
 
-    }
+        //Used to find the current day and decorate it
+        materialCalendarView.addDecorator(new DayViewDecorator() {
+            @Override
+            public boolean shouldDecorate(CalendarDay day) {
+                Calendar cal1 = day.getCalendar();
+                Calendar cal2 = Calendar.getInstance();
 
+                return (cal1.get(Calendar.ERA) == cal2.get(Calendar.ERA)
+                        && cal1.get(Calendar.YEAR) == cal2.get(Calendar.YEAR)
+                        && cal1.get(Calendar.DAY_OF_YEAR) ==
+                        cal2.get(Calendar.DAY_OF_YEAR));
+            }
+
+            @Override
+            public void decorate(DayViewFacade view) {
+                view.addSpan(new ForegroundColorSpan(ContextCompat.getColor(getActivity().getApplicationContext(), R.color.colorDarkBlue))); //sets the color
+                view.addSpan(new StyleSpan(Typeface.BOLD)); //sets the current day to a BOLD text style
+                view.addSpan(new RelativeSizeSpan(1.25f)); //sets the fontsize to be a little bigger than the other days.
+            }
+        });
+
+    }
 }
